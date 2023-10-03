3494a2823ceca017950083545bf14a1e3c386202
Himank Tyagi <68347821+Himank289@users.noreply.github.com>
2023-10-03 23:18:22 +0530

    Add files via upload

---
+++ desktop1.js
@@ -0,0 +1,407 @@
+import React from 'react'
+
+import { Helmet } from 'react-helmet'
+
+import './desktop1.css'
+
+const Desktop1 = (props) => {
+  return (
+    <div className="desktop1-container">
+      <Helmet>
+        <title>exported project</title>
+      </Helmet>
+      <div className="desktop1-desktop1">
+        <img alt="Rectangle31127" src className="desktop1-rectangle31" />
+        <img alt="Rectangle128" src className="desktop1-rectangle" />
+        <div className="desktop1-group15">
+          <img alt="Rectangle30130" src className="desktop1-rectangle30" />
+          <img
+            alt="Star3131"
+            src="/external/star3131-13zo.svg"
+            className="desktop1-star3"
+          />
+          <img
+            alt="jessicaradanavongIchPBHFD0pwunsplash132"
+            src="https://s3-alpha-sig.figma.com/img/2129/d70b/4de9b05701303b560c9103908d936cb1?Expires=1696809600&Signature=PsPKpXImyPFUV1OhL-mJTcUKYZWe4jVVT8ECdRH02Gwkf0K7olPWv2bGp1fvJw7uHHR3gFY7TPOZw-DWotHZ6DpUvGh6cEcclWitpzIsFfx1PCdgDZk7lXmziA4-l4MEE5vz4oB~yIHDJmVnuGWzgXKQr4ylXA0AiiJ2tgd2hZmIHwJfElzvtZbfEjxWJaO83pjAdr~fKKCP5ZSEWwiP6Lai4dVj9gdgCXI6f7nrTQqTHhuYni~Tui4xT0yDH2~P2CPWKmhPHpR8Zm6y9z8dVe33r8rZEjLUkJ3tbRi8fiTNnFiHLVGjVjXlUPtKhQ6rZFtRA01OW3Y7H74ZGiGYGQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            className="desktop1-jessicaradanavong-ich-pbhfd0pwunsplash"
+          />
+          <img alt="Rectangle29133" src className="desktop1-rectangle29" />
+          <img
+            alt="Star4134"
+            src="/Assignment/public/external/star4134-8en.svg"
+            className="desktop1-star4"
+          />
+          <span className="desktop1-text">
+            <span>Oregon jacket</span>
+          </span>
+          <span className="desktop1-text02">
+            <span>$124</span>
+          </span>
+          <img
+            alt="Line23137"
+            src="https://s3-alpha-sig.figma.com/img/18dd/a13e/07d18c9e3059f7e9658764387e84c47e?Expires=1696809600&Signature=J6k1wDPAS4VZjA9O5x5c4LrGeqp9im~~ToGyDhHVDIux~gTl~CVKDuDNsujlXwWIqZUJJTnD9W5a6Z8qn6ngJrrc7TqeM0o49JTnN6iLudDP0R768-FZ8knceF0FIRgkl0iUN427qyUNGWI7y0ITmwOBR1CuTFhERxUbCxywFR76r9skmxdmdVQxAs6jE~6-H92EZHuvzZBd9J~MfzMgyUQrK128zwfGFvoPhL5OvIOd3mrADZ5QfAIqltBZinDGH~n3Nkr3eLcV6lOWt5OotM52-LtNMWWFftDyLw8AS~coQV~edbZgWJISjBhFmM8jgXk1YijGlj0iq2DxD0HK0A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+            className="desktop1-line23"
+          />
+        </div>
+        <img
+          alt="zamnungarayacw3b7q6Ys0unsplash138"
+          src="https://s3-alpha-sig.figma.com/img/c720/4cb2/04eb2b299e5bbdcf1dc998f1a18fc075?Expires=1696809600&Signature=fXhk4DXGXmwYzdnXWaoFV7qbyDnSyN02-oT~uUEe2ALtOECscScB6suCVdz~1frKruVxpmUrRYhOarilkz0Gzj3-~qXbWG0~gtlmstBBWY1S~T34Pak4G7KbNOnWBjAXiTaCR9qBqg06xdhkrkc40hult8J3hkXnU8BrrlWvcis~oFanVsTCXWdlm~7cTnI2cHn0aOkFgCYlTNT7K~YCzt46FzjR~0IuZHQ3LC4D8XzA4YA8w4CnUKLF29KjVXM0w~e238I828YycVwb0BCujprBO7jyMA8sK~JsvATZtxRSof75hSupBpP38SDsxTtKPkgGsu1epTJLZrYOtLS0TA__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          className="desktop1-zamnungarayacw3b7q6ys0unsplash"
+        />
+        <img
+          alt="philipparltNmH9A0obon8unsplash139"
+          src="https://s3-alpha-sig.figma.com/img/18dd/a13e/07d18c9e3059f7e9658764387e84c47e?Expires=1696809600&Signature=J6k1wDPAS4VZjA9O5x5c4LrGeqp9im~~ToGyDhHVDIux~gTl~CVKDuDNsujlXwWIqZUJJTnD9W5a6Z8qn6ngJrrc7TqeM0o49JTnN6iLudDP0R768-FZ8knceF0FIRgkl0iUN427qyUNGWI7y0ITmwOBR1CuTFhERxUbCxywFR76r9skmxdmdVQxAs6jE~6-H92EZHuvzZBd9J~MfzMgyUQrK128zwfGFvoPhL5OvIOd3mrADZ5QfAIqltBZinDGH~n3Nkr3eLcV6lOWt5OotM52-LtNMWWFftDyLw8AS~coQV~edbZgWJISjBhFmM8jgXk1YijGlj0iq2DxD0HK0A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          className="desktop1-philipparlt-nm-h9a0obon8unsplash"
+        />
+        <img
+          alt="philipparlt8EyBRvaWtYunsplash140"
+          src="https://s3-alpha-sig.figma.com/img/18dd/a13e/07d18c9e3059f7e9658764387e84c47e?Expires=1696809600&Signature=J6k1wDPAS4VZjA9O5x5c4LrGeqp9im~~ToGyDhHVDIux~gTl~CVKDuDNsujlXwWIqZUJJTnD9W5a6Z8qn6ngJrrc7TqeM0o49JTnN6iLudDP0R768-FZ8knceF0FIRgkl0iUN427qyUNGWI7y0ITmwOBR1CuTFhERxUbCxywFR76r9skmxdmdVQxAs6jE~6-H92EZHuvzZBd9J~MfzMgyUQrK128zwfGFvoPhL5OvIOd3mrADZ5QfAIqltBZinDGH~n3Nkr3eLcV6lOWt5OotM52-LtNMWWFftDyLw8AS~coQV~edbZgWJISjBhFmM8jgXk1YijGlj0iq2DxD0HK0A__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4"
+          className="desktop1-philipparlt8ey-brva-wt-yunsplash"
+        />
+        <span className="desktop1-text04 H1title">
+          <span>Fresh</span>
+        </span>
+        <span className="desktop1-text06 H2title">
+          <span>New products</span>
+        </span>
+        <span className="desktop1-text08 H1title">
+          <span>Look</span>
+        </span>
+        <span className="desktop1-text10 H1title">
+          <span>2022</span>
+        </span>
+        <span className="desktop1-text12 Bodytext18">
+          <span>Apparel</span>
+        </span>
+        <span className="desktop1-text14 Subtitles">
+          <span>Accessories</span>
+        </span>
+        <span className="desktop1-text16 Bodytext18">
+          <span>Best sellers</span>
+        </span>
+        <span className="desktop1-text18 Bodytext18">
+          <span>50% off</span>
+        </span>
+        <img alt="Rectangle27149" src className="desktop1-rectangle27" />
+        <img alt="Rectangle28150" src className="desktop1-rectangle28" />
+        <img alt="IMAGE18591063151" src className="desktop1-image18591063" />
+        <span className="desktop1-text20">
+          <span>HOME</span>
+        </span>
+        <span className="desktop1-text22">
+          <span>ABOUT</span>
+        </span>
+        <span className="desktop1-text24">
+          <span>Copyright 2022 All Right Reserved By SG</span>
+        </span>
+        <span className="desktop1-text26">
+          <span>CONTACT US</span>
+        </span>
+        <span className="desktop1-text28 Bodytext14">
+          <span>BAG (0)</span>
+        </span>
+        <span className="desktop1-text30 Bodytext14">
+          <span>WISHLIST (0)</span>
+        </span>
+        <span className="desktop1-text32">
+          <span>ShopKart</span>
+        </span>
+        <span className="desktop1-text34">
+          <span>Free Delivery</span>
+        </span>
+        <span className="desktop1-text36">
+          <span>Return Policy</span>
+        </span>
+        <span className="desktop1-text38">
+          <span>Login</span>
+        </span>
+        <span className="desktop1-text40">
+          <span>Follow US</span>
+        </span>
+        <div className="desktop1-layer156">
+          <img
+            alt="Vector164"
+            src="/external/vector164-sxig.svg"
+            className="desktop1-vector"
+          />
+        </div>
+        <img
+          alt="Vector165"
+          src="/external/vector165-7w5.svg"
+          className="desktop1-vector01"
+        />
+        <img
+          alt="Vector166"
+          src="/external/vector166-frd.svg"
+          className="desktop1-vector02"
+        />
+        <img
+          alt="Vector167"
+          src="/external/vector167-8ioa.svg"
+          className="desktop1-vector03"
+        />
+        <div className="desktop1-group6">
+          <div className="desktop1-group">
+            <img
+              alt="Vector170"
+              src="/external/vector170-dvx.svg"
+              className="desktop1-vector04"
+            />
+            <img
+              alt="Vector171"
+              src="/external/vector171-32cs.svg"
+              className="desktop1-vector05"
+            />
+            <img
+              alt="Vector172"
+              src="/external/vector172-b4e.svg"
+              className="desktop1-vector06"
+            />
+          </div>
+          <img
+            alt="Vector173"
+            src="/external/vector173-jb19.svg"
+            className="desktop1-vector07"
+          />
+        </div>
+        <img
+          alt="Line11174"
+          src="/external/line11174-f388.svg"
+          className="desktop1-line11"
+        />
+        <div className="desktop1-group16">
+          <img
+            alt="Line17176"
+            src="/external/line17176-alnf.svg"
+            className="desktop1-line17"
+          />
+          <img
+            alt="Star1177"
+            src="/external/star1177-vlmd.svg"
+            className="desktop1-star1"
+          />
+        </div>
+        <div className="desktop1-group17">
+          <img
+            alt="Line18179"
+            src="/external/line18179-bbn9.svg"
+            className="desktop1-line18"
+          />
+          <img
+            alt="Star2180"
+            src="/external/star2180-2y9y.svg"
+            className="desktop1-star2"
+          />
+        </div>
+        <span className="desktop1-text42 Bodytext18">
+          <span>
+            <span></span>
+            <br></br>
+            <span>Get news about articles and updates in your inbox.</span>
+          </span>
+        </span>
+        <span className="desktop1-text47 Subtitles">
+          <span>Newslleter</span>
+        </span>
+        <span className="desktop1-text49 Bodytext18">
+          <span>NAME</span>
+        </span>
+        <span className="desktop1-text51 Bodytext18">
+          <span>EMAIL</span>
+        </span>
+        <span className="desktop1-text53 Bodytext18">
+          <span>MESSAGE</span>
+        </span>
+        <img
+          alt="Arrow1186"
+          src="/external/arrow1186-hy5o.svg"
+          className="desktop1-arrow1"
+        />
+        <img
+          alt="Arrow2187"
+          src="/external/arrow2187-6h2c.svg"
+          className="desktop1-arrow2"
+        />
+        <span className="desktop1-text55 H1title">
+          <span>
+            <span>
+              GET
+              <span
+                dangerouslySetInnerHTML={{
+                  __html: ' ',
+                }}
+              />
+            </span>
+            <br></br>
+            <span>IN TOUCH</span>
+          </span>
+        </span>
+        <img
+          alt="Line19189"
+          src="/external/line19189-gh2m.svg"
+          className="desktop1-line19"
+        />
+        <img
+          alt="Line20190"
+          src="/external/line20190-0w4jv.svg"
+          className="desktop1-line20"
+        />
+        <img
+          alt="Line21191"
+          src="/external/line21191-x6w8.svg"
+          className="desktop1-line21"
+        />
+        <div className="desktop1-button">
+          <img alt="Ellipse1193" src className="desktop1-ellipse1" />
+          <span className="desktop1-text60">
+            <span>SEND</span>
+          </span>
+        </div>
+        <div className="desktop1-group19">
+          <img alt="Rectangle18I195" src className="desktop1-rectangle18" />
+          <img
+            alt="VectorI195"
+            src="/external/vectori195-ga89.svg"
+            className="desktop1-vector08"
+          />
+        </div>
+        <button className="desktop1-secondarybutton">
+          <img alt="Rectangle18I196" src className="desktop1-rectangle181" />
+          <img
+            alt="VectorI196"
+            src="/external/vectori196-wav.svg"
+            className="desktop1-vector09"
+          />
+        </button>
+        <button className="desktop1-secondarybutton1">
+          <img alt="Rectangle18I197" src className="desktop1-rectangle182" />
+          <img
+            alt="VectorI197"
+            src="/external/vectori197-xvb.svg"
+            className="desktop1-vector10"
+          />
+        </button>
+        <img
+          alt="malickimbeserPKMvkg7vnUounsplash198"
+          src
+          className="desktop1-malickimbeser-pk-mvkg7vn-uounsplash"
+        />
+        <button className="desktop1-secondarybutton2">
+          <img alt="Rectangle18I199" src className="desktop1-rectangle183" />
+          <img
+            alt="VectorI199"
+            src="/external/vectori199-h1i.svg"
+            className="desktop1-vector11"
+          />
+        </button>
+        <div className="desktop1-frame11197">
+          <div className="desktop1-frame1">
+            <div className="desktop1-frame3">
+              <span className="desktop1-text62">
+                <span>See more</span>
+              </span>
+            </div>
+            <img
+              alt="VectorI110"
+              src="/external/vectori110-tz06.svg"
+              className="desktop1-vector12"
+            />
+          </div>
+          <img
+            alt="Line16I110"
+            src="/external/line16i110-s3co.svg"
+            className="desktop1-line16"
+          />
+        </div>
+        <div className="desktop1-dropdown">
+          <span className="desktop1-text64">
+            <span>OUR PRODUCTS</span>
+          </span>
+          <span className="desktop1-text66">
+            <span>PRODUCT 1</span>
+          </span>
+          <span className="desktop1-text68">
+            <span>PRODUCT 2</span>
+          </span>
+          <span className="desktop1-text70">
+            <span>PRODUCT 3</span>
+          </span>
+          <span className="desktop1-text72">
+            <span>PRODUCT 4</span>
+          </span>
+          <img
+            alt="Line34I110"
+            src="/external/line34i110-eg3r.svg"
+            className="desktop1-line34"
+          />
+        </div>
+        <div className="desktop1-textfield">
+          <div className="desktop1-frame11194">
+            <span className="desktop1-text74 Subtitles">
+              <span>FLORIDA JACKET</span>
+            </span>
+            <span className="desktop1-text76 Bodytext14">
+              <span>
+                Suffered alteration in some form, bysuffalterattion in some
+                forme, byinjected humor, or randomised
+              </span>
+            </span>
+            <span className="desktop1-text78 Subtitles">
+              <span>$100</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-textfield1">
+          <div className="desktop1-frame111941">
+            <span className="desktop1-text80 Subtitles">
+              <span>FLORIDA JACKET</span>
+            </span>
+            <span className="desktop1-text82 Bodytext14">
+              <span>
+                Suffered alteration in some form, bysuffalterattion in some
+                forme, byinjected humor, or randomised
+              </span>
+            </span>
+            <span className="desktop1-text84 Subtitles">
+              <span>$100</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-textfield2">
+          <div className="desktop1-frame111942">
+            <span className="desktop1-text86 Subtitles">
+              <span>FLORIDA JACKET</span>
+            </span>
+            <span className="desktop1-text88 Bodytext14">
+              <span>
+                Suffered alteration in some form, bysuffalterattion in some
+                forme, byinjected humor, or randomised
+              </span>
+            </span>
+            <span className="desktop1-text90 Subtitles">
+              <span>$100</span>
+            </span>
+          </div>
+        </div>
+        <div className="desktop1-textfield3">
+          <div className="desktop1-frame111943">
+            <span className="desktop1-text92 Subtitles">
+              <span>FLORIDA JACKET</span>
+            </span>
+            <span className="desktop1-text94 Bodytext14">
+              <span>
+                Suffered alteration in some form, bysuffalterattion in some
+                forme, byinjected humor, or randomised
+              </span>
+            </span>
+            <span className="desktop1-text96 Subtitles">
+              <span>$100</span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Desktop1
